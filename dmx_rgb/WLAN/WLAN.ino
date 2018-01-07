02969b68cd394a7981a9961326c5899d18c9324a
Sebastian <sebastian.mundner@gmail.com>
2018-01-07 13:02:46 +0100

    clean up
    Cleand not needed files and rename the WiFi sketch

---
+++ dmx_rgb/WLAN/WLAN.ino
@@ -0,0 +1,45 @@
+#include <ESP8266WiFi.h>
+#include <WiFiUDP.h>
+
+const char* ssid = "Krypto";
+const char* password = "AnDeZe20Mu";
+
+WiFiServer server(5999);
+WiFiUDP udp;
+WiFiClient sclient;
+
+void setup() {
+  Serial.begin(250000);
+  //WiFi.mode(WIFI_STA);
+  //WiFi.begin(ssid,password);
+  WiFi.softAP(ssid, password);
+  server.begin();
+  server.setNoDelay(true);
+  udp.begin(5998);
+}
+
+void loop() {
+  int isUdp = udp.parsePacket();
+  if (isUdp) {
+    IPAddress remote = udp.remoteIP();
+
+    udp.beginPacket(remote, 5998);
+    udp.write("DMX", 3);
+    udp.endPacket();
+  }
+
+  if (server.hasClient()) {
+    sclient = server.available();
+    //Serial.println("client connected");
+  }
+  if ( sclient.available())
+  {
+    //Serial.println("Data send");
+    byte buff[3];
+    sclient.read(buff, 3);
+    Serial.write(buff, 3);
+    sclient.stop();
+  }
+
+
+}
