f5e8a1d6193d9a67b667050c6fe821c67dab22ca
Sebastian <sebastian.mundner@gmail.com>
2018-08-24 14:22:14 +0200

    spannungs wandler getauscht

--- dmx_rgb/serialTest.py
+++ dmx_rgb/serialTest.py
@@ -1,6 +1,7 @@
 import serial
-ser = serial.Serial('COM5',9600)
+ser = serial.Serial('COM5',250000)
+ser.parity = serial.PARITY_NONE
+ser.stopbits = serial.STOPBITS_TWO
 
-
-while True:
-    print(int.from_bytes(ser.read(),byteorder='big'))
+ser.write(bytes([0,255,0]))
+ser.close()
